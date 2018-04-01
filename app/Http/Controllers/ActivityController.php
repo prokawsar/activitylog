aedb54fc78e3b7f23ccae61dd94a6818df559c13
Kawsar Ahmed <mkawsar10@gmail.com>
2018-04-01 13:10:42 +0600

    home page element added

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class ActivityController extends Controller
 {
-    //
+    public function __construct()
+    {
+        return $this->middleware('auth');
+    }
+
+
+    public function addActivity(Request $request)
+    {
+
+        return redirect(route('home'))->with('status', 'Activity Added');
+    }
+
+
+    public function disableActivity($id)
+    {
+
+        return redirect(route('home'))->with('status', 'Activity Disabled');
+    }
+
+
+    public function deleteActivity($id)
+    {
+
+        return redirect(route('home'))->with('status', 'Activity Deleted');
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,66 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10 col-md-offset-1">
+                <div class="card">
+                    <div class="card-header">
+                        Dashboard
+                        <span class="float-right">
+                            {{ \Carbon\Carbon::today()->toDateString() }}
+                        </span>
+                    </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <div class="row justify-content-center">
+                            <div class="col-md-6">
+                                {{--<h3>Current Activity </h3>--}}
+                                <table class="table table-hover">
+                                    <thead>
+                                    <tr>
+                                        <th class="text-center">Current Activity</th>
+                                        <th></th>
+
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td>John</td>
+                                        <td><input type="checkbox"></td>
+                                    </tr>
+
+                                    </tbody>
+                                </table>
+
+                            </div>
                         </div>
-                    @endif
+                        <div class="row justify-content-center">
+                            <div class="col-md-6 col-md-offset-3">
+                                <form action="#" method="post">
+                                    {{ csrf_field() }}
+                                    <div class="jumbotron">
 
-                    You are logged in!
+                                        <div class="form-group">
+                                            <label for="activity">Activity name:</label>
+                                            <input type="text" class="form-control" id="activity">
+                                        </div>
+
+
+                                        <button type="submit" class="btn btn-primary">Add</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
